4a011828de460431c1b0505b0149b48d32293ce3
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-03-23 00:45:39 +0530

    Update LED_roll.lua

--- libraries/AP_Scripting/examples/LED_roll.lua
+++ libraries/AP_Scripting/examples/LED_roll.lua
@@ -6,7 +6,7 @@ the LED interface for WS2812 LEDs
 --[[
 for this demo we will use a single strip with 30 LEDs
 --]]
-local num_leds = 1
+local num_leds = 4
 
 --[[
  use SERVOn_FUNCTION 120 for LED. We can control up to 16 separate strips of LEDs
