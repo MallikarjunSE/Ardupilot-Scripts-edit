96465cc7279193cd83653bb02779b0036d419683
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-03-23 00:30:14 +0530

    Update simple_loop.lua

--- libraries/AP_Scripting/examples/simple_loop.lua
+++ libraries/AP_Scripting/examples/simple_loop.lua
@@ -2,7 +2,7 @@
 
 function update() -- this is the loop which periodically runs
   gcs:send_text(0, "Welcome to Sierra Aerospace") -- send the traditional message
-  return update, 1000 -- reschedules the loop
+  return update, 3 -- reschedules the loop
 end
 
 return update() -- run immediately before starting to reschedule
