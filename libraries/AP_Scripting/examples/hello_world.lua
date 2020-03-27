acf573ada6ee9934d55ac1d56b5cb7104e81b65b
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-03-27 19:48:30 +0530

    Update hello_world.lua

--- libraries/AP_Scripting/examples/hello_world.lua
+++ libraries/AP_Scripting/examples/hello_world.lua
@@ -1,21 +1,12 @@
-local count = 0
+local TUNE_POINT = "MBNT255>A#8A#8A#8A#8A#8A#8A#8A#8A#8A#8A#8A#8A#8A#8A#8A#8"
+local TUNE_TOWARDS = "MFT100L8>B"
+local TUNE_AWAY = "MFT100L4>A#B#"
 
-function update() -- this is the loop which periodically runs
-
-  count = count + 1
-
-  if count == 1 then
-    --star wars
-    notify:play_tune(T200L4O4FO4FO4FP16MNL2O4BO5L8O5EO5DO5CL2O5B)
-  elseif count == 2 then
-    -- ending
-    notify:play_tune(L4O5FL8O5E-O5DO5CL2O5B-L4O5FL8O5E-O5DO5E-L1O5C)
-        count = 0
-  end
-
-  return update, 15000 -- reschedules the loop in 15 seconds
-
-end
-
-return update()
-
+function update ()
+    local current = ahrs:get_roll()
+    if current > 30 then
+            notify:play_tune(TUNE_POINT)
+            elseif current <-30  then
+                notify:play_tune(TUNE_AWAY)
+            end
+        end
