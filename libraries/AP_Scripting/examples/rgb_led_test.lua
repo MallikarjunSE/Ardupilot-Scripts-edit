ffa89fcc25fd51e2335c9ca4a39d3e1b18fdb8b4
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-03-29 22:24:39 +0530

    Update rgb_led_test.lua

--- libraries/AP_Scripting/examples/rgb_led_test.lua
+++ libraries/AP_Scripting/examples/rgb_led_test.lua
@@ -8,33 +8,33 @@ function update() -- this is the loop which periodically runs
 
   if count == 1 then
     -- solid red
-        gcs:send_text(0, "Red")
+        gcs:send_text(0, "Code Red")
     -- red,green,blue,rate hz, duration ms
-    notify:handle_rgb(255,0,0,5)
+    notify:handle_rgb(255,0,0,2)
   elseif count == 2 then
     -- solid green
-        gcs:send_text(0, "Green")
-    notify:handle_rgb(0,255,0,5)
+        gcs:send_text(0, "Code Green")
+    notify:handle_rgb(0,255,0,2)
   elseif count == 3 then
     -- solid blue
-        gcs:send_text(0, "Blue")
-    notify:handle_rgb(0,0,255,5)
+        gcs:send_text(0, "Code Blue")
+    notify:handle_rgb(0,0,255,2)
   elseif count == 4 then
     -- 1hz red  + green
         gcs:send_text(0, "Red+Green")
-    notify:handle_rgb(255,255,0,5)
+    notify:handle_rgb(255,255,0,2)
   elseif count == 5 then
     -- 1hz green + blue
         gcs:send_text(0, "Green+Blue")
-    notify:handle_rgb(0,255,255,5)
+    notify:handle_rgb(0,255,255,2)
   elseif count == 6 then
     -- 1hz red + blue
         gcs:send_text(0, "Red+Blue")
-    notify:handle_rgb(255,0,255,5)
+    notify:handle_rgb(255,0,255,2)
   elseif count == 7 then
     -- fast white
-        gcs:send_text(0, "White")
-    notify:handle_rgb(255,255,255,5)
+        gcs:send_text(0, "Code White")
+    notify:handle_rgb(255,255,255,2)
     count = 0
   end
 
