dd6c3d328241a48c4223cd09d8e4c24c319d9bd4
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-03-29 22:21:17 +0530

    Update rgb_led_test.lua

--- libraries/AP_Scripting/examples/rgb_led_test.lua
+++ libraries/AP_Scripting/examples/rgb_led_test.lua
@@ -8,25 +8,32 @@ function update() -- this is the loop which periodically runs
 
   if count == 1 then
     -- solid red
+        gcs:send_text(0, "Red")
     -- red,green,blue,rate hz, duration ms
     notify:handle_rgb(255,0,0,5)
   elseif count == 2 then
     -- solid green
+        gcs:send_text(0, "Green")
     notify:handle_rgb(0,255,0,5)
   elseif count == 3 then
     -- solid blue
+        gcs:send_text(0, "Blue")
     notify:handle_rgb(0,0,255,5)
   elseif count == 4 then
     -- 1hz red  + green
+        gcs:send_text(0, "Red+Green")
     notify:handle_rgb(255,255,0,5)
   elseif count == 5 then
     -- 1hz green + blue
+        gcs:send_text(0, "Green+Blue")
     notify:handle_rgb(0,255,255,5)
   elseif count == 6 then
     -- 1hz red + blue
+        gcs:send_text(0, "Red+Blue")
     notify:handle_rgb(255,0,255,5)
   elseif count == 7 then
     -- fast white
+        gcs:send_text(0, "White")
     notify:handle_rgb(255,255,255,5)
     count = 0
   end
