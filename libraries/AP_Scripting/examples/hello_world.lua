49f9291a0dd33e582ad4b8d4d95662f997567361
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-03-30 18:33:31 +0530

    Update hello_world.lua

--- libraries/AP_Scripting/examples/hello_world.lua
+++ libraries/AP_Scripting/examples/hello_world.lua
@@ -2,9 +2,9 @@ current = ahrs:prearm_healthy()
 
 function update()
     if current==true then 
-        notify:handle_rgb(0,255,0,2)
+        notify:handle_rgb(0,255,0,5)
         elseif current==false then
-        notify:handle_rgb(255,0,0,2)
+        notify:handle_rgb(255,0,0,5)
         end
     return update, 15000
     end
