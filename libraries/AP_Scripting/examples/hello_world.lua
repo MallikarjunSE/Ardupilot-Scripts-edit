29bc186a28bc03a9abcd222422ab75256329ec1e
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-03-30 18:43:31 +0530

    Update hello_world.lua

--- libraries/AP_Scripting/examples/hello_world.lua
+++ libraries/AP_Scripting/examples/hello_world.lua
@@ -4,7 +4,7 @@ function update()
     if current==false then 
         notify:handle_rgb(0,255,0,5)
         elseif current==true then
-        notify:handle_rgb(255,0,0,5)
+        notify:handle_rgb(0,0,255,5)
         end
     return update, 15000
     end
