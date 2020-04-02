d14a1672c1399cd8e7d8b41f26d73af648d8f9e8
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-04-03 00:58:39 +0530

    Update hello_world.lua

--- libraries/AP_Scripting/examples/hello_world.lua
+++ libraries/AP_Scripting/examples/hello_world.lua
@@ -5,7 +5,7 @@ local TUNE_AWAY = "MFT100L4>A#B#"
 roll = ahrs:get_roll()
 pitch = ahrs:get_pitch()
 
-function upadte()
+function update()
     if roll > 0.5 then
         notify:play_tune(TUNE_POINT)
         gcs:send_text(1, "High Roll Angle")
