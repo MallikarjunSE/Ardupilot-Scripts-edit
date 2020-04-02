b0d3d575cf3a26c4f535e23ad372c5d6a878c4e2
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-04-03 00:11:01 +0530

    Update hello_world.lua

--- libraries/AP_Scripting/examples/hello_world.lua
+++ libraries/AP_Scripting/examples/hello_world.lua
@@ -3,8 +3,10 @@ Health = ahrs:prearm_healthy()
 function update()
     if health==true then 
         notify:handle_rgb(0,255,0,10)
+        send_text(1, "Vehicle read to fly")
         else
         notify:handle_rgb(255,0,0,10)
+        send_text(1, "AHRS not healthy")
         end
     return update, 15000
     end
