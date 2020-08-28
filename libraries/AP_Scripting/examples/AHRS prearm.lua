065b35e9060f03523da1739199fbbc89738f1f43
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-08-28 15:37:15 +0530

    Update AHRS prearm.lua

--- libraries/AP_Scripting/examples/AHRS prearm.lua
+++ libraries/AP_Scripting/examples/AHRS prearm.lua
@@ -1,6 +1,6 @@
 Health = ahrs:prearm_healthy()
 local num_leds = 4
-localchan = SRV_channels:find_channel(94)
+local chan = SRV_channels:find_channel(94)
 if not chan then
     gcs:send_tet(6, "LEDs:channel not set")
     return
@@ -8,12 +8,13 @@ if not chan then
 chan=chan+1
 gcs:send_text(6, "LEDs:chan=" ..tostring(chan))
 serialLED:set_num_neopixel(chan, num_leds)
+serialLED:set_RGB(chan, led, r, g, b)
 
 function update()
     if health==true then 
-        serialLED:set_rgb(0,255,0,10)
+        serialLED:set_rgb(chan,1,255,0,0)
         else
-        serialLED:set_rgb(255,0,0,10)
+        serialLED:set_rgb(chan,2,0,0,255)
         gcs:send_text(1, "Wait for AHRS to settle")
         end
     return update, 15000
