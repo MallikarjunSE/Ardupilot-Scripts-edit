796a297a44241ba7191f42720815e2edf1eb8beb
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2020-08-28 14:44:25 +0530

    Update AHRS prearm.lua

--- libraries/AP_Scripting/examples/AHRS prearm.lua
+++ libraries/AP_Scripting/examples/AHRS prearm.lua
@@ -2,10 +2,9 @@ Health = ahrs:prearm_healthy()
 
 function update()
     if health==true then 
-        notify:handle_rgb(0,255,0,10)
-        gcs:send_text(1, "Vehicle ready to fly")
+        serialLED:set_rgb(0,255,0,10)
         else
-        notify:handle_rgb(255,0,0,10)
+        serialLED:set_rgb(255,0,0,10)
         gcs:send_text(1, "Wait for AHRS to settle")
         end
     return update, 15000
