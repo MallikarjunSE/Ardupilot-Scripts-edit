821db48950b0f41de2e6a4810888573ae63c32cc
MallikarjunSE <49279505+MallikarjunSE@users.noreply.github.com>
2021-02-11 14:18:26 +0530

    Update rgb_led_test.lua

--- libraries/AP_Scripting/examples/rgb_led_test.lua
+++ libraries/AP_Scripting/examples/rgb_led_test.lua
@@ -7,9 +7,6 @@ function update() -- this is the loop which periodically runs
   count = count + 1
 
   if count == 1 then
-    -- solid red
-        gcs:send_text(0, "Code Red")
-    -- red,green,blue,rate hz, duration ms
     notify:handle_rgb(255,255,255,10)
       count = 0
   end
